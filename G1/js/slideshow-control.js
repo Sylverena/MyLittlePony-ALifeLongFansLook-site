58098304fe8428b76b12ee55f7aa1552649a9f4e
sylverena <serenazsyl@gmail.com>
2024-04-30 12:48:44 -0500

    SLIDESHOW ADDED YE GODS

---
+++ G1/js/slideshow-control.js
@@ -0,0 +1,23 @@
+$(document).ready( function () {
+    $('.char-slideshow').lightSlider({
+        item: 3,
+        slideMargin: 20,
+        slideMove: 1,
+        loop: true,
+        enableDrag: false,
+        auto: true,
+        pauseOnHover: true,
+        pause: 3500
+    });
+
+    $('.toy-slideshow').lightSlider({
+        item: 3,
+        slideMargin: 20,
+        slideMove: 1,
+        loop: true,
+        enableDrag: false,
+        auto: true,
+        pauseOnHover: true,
+        pause: 3500
+    });
+})
